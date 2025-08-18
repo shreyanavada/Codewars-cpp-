43578a74675d38c605941cd0a3888ffd1e7b8ba1
Shreya Navada <shreyanavada26@gmail.com>
2025-08-18 23:48:24 +0530

    Create solution.cpp

---
+++ logical katas/Square(n) Sum/solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+int square_sum(const std::vector<int>& numbers)
+{
+int sum=0;
+for(int i=0 ; i<numbers.size();i++)
+{
+sum += (numbers[i])*(numbers[i]);
+}
+
+    return sum;
+}
