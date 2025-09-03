429de107cde230687b8f7a76c6c97bfb2301498e
Shreya Navada <shreyanavada26@gmail.com>
2025-09-03 23:55:57 +0530

    Create solution.cpp

---
+++ logical katas/Sum of positive/solution.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+int positive_sum (const std::vector<int> arr){
+  int sum=0;
+  for(int i=0 ;i<arr.size() ;i++)
+    {
+    if(arr[i]>0)
+      {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
