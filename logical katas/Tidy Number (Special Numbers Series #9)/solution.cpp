f817730c529431d34dafcba8e3dd42be927b7808
Shreya Navada <shreyanavada26@gmail.com>
2025-09-05 22:59:46 +0530

    Create solution.cpp

---
+++ logical katas/Tidy Number (Special Numbers Series #9)/solution.cpp
@@ -0,0 +1,21 @@
+//brute force method : where it iterates through each element
+
+#include <string>
+using namespace std; 
+
+bool tidyNumber (int number)
+{
+  string n =to_string(number);
+  for(int i =0 ;i< n.size()-1 ;i++)
+    {
+    if(n[i]>n[i+1])
+      {
+      return false;
+    }
+    
+  }
+  
+  return true ;
+}
+
+// time complexity : O(n)   space complexity : O(n) .
